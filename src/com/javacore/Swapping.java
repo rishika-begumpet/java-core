3e64ca28186ef404703901fe30f4edc78f3df5c7
rishika-begumpet <begumpetrishika@gmail.com>
2026-09-09 20:55:38 +0530

    Swapping values

---
+++ src/com/javacore/Swapping.java
@@ -0,0 +1,17 @@
+package com.javacore;
+import java.util.Scanner;
+
+public class Swapping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value a :");
+int a = sc.nextInt();
+        System.out.println("Enter value b :");
+int b = sc.nextInt();
+a = a + b ;
+b = a - b;
+a = a - b;
+System.out.println("Value of a: " + a);
+System.out.println("Value of b:" + b);
+    }
+}
